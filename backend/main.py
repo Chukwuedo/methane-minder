d2404cb885dfb3a51f66f64cb3851f79ad582be3
Chukwudum Chukwuedo <kriz2phorus@gmail.com>
2025-06-23 12:57:16 +0100

    chore: Move backend subfolders 'app' and 'docs' into 'src' subfolder

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.v1.endpoints import plumes
+from src.app.api.v1.endpoints import plumes
 
 app = FastAPI(
     title="Methane Minder Backend",
